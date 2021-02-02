a15f38163ec3af8fa06227b87bb86d22f8502a09
Xavier Hazzard <xavier@hazzard.org.uk>
2021-02-02 13:16:57 +0000

    Fifth Commit

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const index = `${__dirname}/public/index.html`;
 
 app.get("/", (req, res) => {
 	res.sendFile(index);
-	o;
 });
 
 app.post("/", (req, res) => {
--- public/app.js
+++ public/app.js
@@ -4,15 +4,33 @@ const addItem = document.querySelector("#addItem");
 addItem.addEventListener("click", () => {
 	const name = document.querySelector("#itemName");
 	const price = document.querySelector("#itemPrice");
+	const imgLink = document.querySelector("#itemImg");
 
 	if (price.value && price.value) {
 		setTimeout(() => {
-			createItem(name.value, price.value, 500);
+			createItem(name.value, price.value, 500, imgLink.value);
+			name.value = "";
+			price.value = "";
+			imgLink.value = "";
 		}, 200);
 	}
 });
 
-function createItem(name, price, budget) {
+// addEventListener("keydown", (e) => {
+// 	const name = document.querySelector("#itemName");
+// 	const price = document.querySelector("#itemPrice");
+// 	const imgLink = document.querySelector("#itemImg");
+// 	if (e.key == "Enter" && price.value && price.value) {
+// 		setTimeout(() => {
+// 			createItem(name.value, price.value, 500, imgLink.value);
+// 			name.value = "";
+// 			price.value = "";
+// 			imgLink.value = "";
+// 		}, 200);
+// 	}
+// });
+
+function createItem(name, price, budget, img) {
 	var div = document.createElement("div");
 	var p1 = document.createElement("p");
 	var p2 = document.createElement("p");
@@ -23,19 +41,33 @@ function createItem(name, price, budget) {
 	p2.classList.add("middle");
 	p3.classList.add("bottom");
 	save.classList.add("saveBtn");
+	// if (img) {
+	// 	console.log(img);
+	// 	var image = document.createElement("img");
+	// 	image.src = img;
+	// 	image.classList.add("img");
+	// 	div.classList.add("pic");
+	// 	div.classList.remove("item");
+	// 	div.appendChild(image);
+	// }
 	var nameSpan = `<span class="value">${name}</span>`;
-	var priceSpan = `<span class="value">${price}</span>`;
-	var savingsSpan = `<span class="value">${budget - price}</span>`;
+	var priceSpan = `<span class="value">£${price}</span>`;
+	var savingsSpan = `<span class="value">£${budget - price}</span>`;
 	p1.innerHTML = `Name: ${nameSpan}`;
 	p2.innerHTML = `Price: ${priceSpan}`;
 	p3.innerHTML = `Savings: ${savingsSpan}`;
-	save.innerHTML = `Save`;
+	save.innerHTML = `<i class="fas fa-trash-alt"></i>`;
 	//add text to each item
 	div.appendChild(p1);
 	div.appendChild(p2);
 	div.appendChild(p3);
 	div.appendChild(save);
 	document.body.appendChild(div);
+	save.addEventListener("click", function () {
+		setTimeout(() => {
+			this.parentElement.remove();
+		}, 250);
+	});
 }
 
 // createItem("1440p Monitor", 400, 0);
