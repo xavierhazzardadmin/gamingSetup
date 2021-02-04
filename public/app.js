12518d57f4454eb8698d2d5fbdf0de0cc6e1bdd7
Xavier Hazzard <xavier@hazzard.org.uk>
2021-02-04 13:49:00 +0000

    Redesigned layout, added total amount to bottom of page

--- public/app.js
+++ public/app.js
@@ -1,5 +1,10 @@
 var btn = document.getElementById("submit");
 const addItem = document.querySelector("#addItem");
+const totalView = document.querySelector("#total");
+// const totalText = document.querySelectorAll(".total")[0];
+const textShow = document.querySelector("#textShow");
+var total = 0;
+var text = false;
 
 // btn.addEventListener("click", (e) => {
 // 	e.preventDefault();
@@ -19,40 +24,50 @@ addItem.addEventListener("click", () => {
 	}
 });
 
-// addEventListener("keydown", (e) => {
-// 	const name = document.querySelector("#itemName");
-// 	const price = document.querySelector("#itemPrice");
-// 	const imgLink = document.querySelector("#itemImg");
-// 	if (e.key == "Enter" && price.value && price.value) {
-// 		setTimeout(() => {
-// 			createItem(name.value, price.value, 500, imgLink.value);
-// 			name.value = "";
-// 			price.value = "";
-// 			imgLink.value = "";
-// 		}, 200);
-// 	}
-// });
+addEventListener("keydown", (e) => {
+	const name = document.querySelector("#itemName");
+	const price = document.querySelector("#itemPrice");
+	const imgLink = document.querySelector("#itemImg");
+	if (e.key == "Enter" && price.value && price.value) {
+		setTimeout(() => {
+			createItem(name.value, price.value, 500, imgLink.value);
+			name.value = "";
+			price.value = "";
+			imgLink.value = "";
+		}, 200);
+	}
+});
+
+function showTotal() {
+	var op = 0;
+	while (op <= 100) {
+		setInterval(() => {
+			console.log("worked");
+		}, 100);
+	}
+}
 
 function createItem(name, price, budget, img) {
+	var body = document.querySelectorAll(".items")[0];
 	var div = document.createElement("div");
 	var p1 = document.createElement("p");
 	var p2 = document.createElement("p");
 	var p3 = document.createElement("p");
 	var save = document.createElement("button");
-	div.classList.add("item", "bgColor");
+	div.classList.add("item");
 	p1.classList.add("top");
 	p2.classList.add("middle");
 	p3.classList.add("bottom");
 	save.classList.add("saveBtn");
-	// if (img) {
-	// 	console.log(img);
-	// 	var image = document.createElement("img");
-	// 	image.src = img;
-	// 	image.classList.add("img");
-	// 	div.classList.add("pic");
-	// 	div.classList.remove("item");
-	// 	div.appendChild(image);
-	// }
+	if (img) {
+		console.log(img);
+		var image = document.createElement("img");
+		image.src = img;
+		image.classList.add("img");
+		div.classList.add("pic");
+		div.classList.remove("item");
+		div.appendChild(image);
+	}
 	var nameSpan = `<span class="value">${name}</span>`;
 	var priceSpan = `<span class="value">£${price}</span>`;
 	var savingsSpan = `<span class="value">£${budget - price}</span>`;
@@ -65,12 +80,17 @@ function createItem(name, price, budget, img) {
 	div.appendChild(p2);
 	div.appendChild(p3);
 	div.appendChild(save);
-	document.body.appendChild(div);
+	body.appendChild(div);
+	total += Number(price);
+	totalView.innerHTML = total;
 	save.addEventListener("click", function () {
 		setTimeout(() => {
 			this.parentElement.remove();
-		}, 250);
+			total -= Number(price);
+			totalView.innerHTML = total;
+		});
 	});
+	textShow.classList.add("show");
 }
 
 // createItem("1440p Monitor", 400, 0);
