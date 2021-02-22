74fe6217ca9387b7751cd8d37c0bc19b20487135
Xavier Hazzard <xavier@hazzard.org.uk>
2021-02-22 11:51:16 +0000

    added eslint and prettier config for writing cleaner code

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5005;
 const bodyparser = require("body-parser");
 
-const options = {};
-var items = [];
-var budget = 5000;
-var total = 0;
-//middleware
-app.use(express.static("public", options));
-app.use(bodyparser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.listen(PORT, console.log(`Server started on ${PORT}`));
+const app = express();
+const PORT = process.env.PORT || 5005;
 
-const index = `${__dirname}/public/index.html`;
+const options = {};
+const items = [];
+let budget = 5000;
+let total = 0;
+//    middleware
+
+app.use(
+    express.static("public", options)
+);
+app.use(
+    bodyparser.urlencoded({
+        extended: true,
+    })
+);
+app.set("view engine", "ejs");
+app.listen(
+    PORT,
+    console.log(
+        `Server started on ${PORT}`
+    )
+);
 
 app.get("/", (req, res) => {
-	res.render("index", { items: items, total: total });
+    res.render("index", {
+        items,
+        total,
+    });
 });
 
 app.post("/additem", (req, res) => {
-	const { Item, Price, itemLink, imgLink } = req.body;
-	total += parseInt(Price);
-	const newItem = {
-		name: Item,
-		cost: Price,
-		link: itemLink,
-		imgLink: imgLink,
-		amountLeft: budget - parseInt(Price),
-	};
+    const {
+        Item,
+        Price,
+        itemLink,
+        imgLink,
+    } = req.body;
+    total += parseInt(Price, 10);
+    const newItem = {
+        name: Item,
+        cost: Price,
+        link: itemLink,
+        imgLink,
+        amountLeft:
+            budget -
+            parseInt(Price, 10),
+    };
 
-	budget -= parseInt(Price);
+    budget -= parseInt(Price, 10);
 
-	items.push(newItem);
-	res.redirect("/");
+    items.push(newItem);
+    res.redirect("/");
 });
--- public/app.js
+++ public/app.js
@@ -1,127 +1,194 @@
-var btn = document.getElementById("submit");
-const addItem = document.querySelector("#addItem");
-const totalView = document.querySelector("#total");
-// const totalText = document.querySelectorAll(".total")[0];
-const textShow = document.querySelector("#textShow");
-var total = 0;
-var savings = 500;
+// var btn = document.getElementById(
+//     "submit"
+// );
+// const addItem = document.querySelector(
+//     "#addItem"
+// );
+// const totalView = document.querySelector(
+//     "#total"
+// );
+// // const totalText = document.querySelectorAll(".total")[0];
+// const textShow = document.querySelector(
+//     "#textShow"
+// );
+// var total = 0;
+// var savings = 500;
 
-const sleep = (milliseconds) => {
-	return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
+// const sleep = (milliseconds) => {
+//     return new Promise((resolve) =>
+//         setTimeout(
+//             resolve,
+//             milliseconds
+//         )
+//     );
+// };
 
-addItem.addEventListener("click", () => {
-	const inputs = document.querySelectorAll(".formInput");
+// addItem.addEventListener(
+//     "click",
+//     () => {
+//         const inputs = document.querySelectorAll(
+//             ".formInput"
+//         );
 
-	setTimeout(() => {
-		createItem(
-			inputs[0].value,
-			inputs[1].value,
-			savings,
-			inputs[2].value,
-			inputs[3].value
-		);
-		inputs[0].value = "";
-		inputs[1].value = "";
-		inputs[2].value = "";
-		inputs[3].value = "";
-	}, 200);
-});
+//         setTimeout(() => {
+//             createItem(
+//                 inputs[0].value,
+//                 inputs[1].value,
+//                 savings,
+//                 inputs[2].value,
+//                 inputs[3].value
+//             );
+//             inputs[0].value = "";
+//             inputs[1].value = "";
+//             inputs[2].value = "";
+//             inputs[3].value = "";
+//         }, 200);
+//     }
+// );
 
-addEventListener("keydown", (e) => {
-	const inputs = document.querySelectorAll(".formInput");
+// addEventListener("keydown", (e) => {
+//     const inputs = document.querySelectorAll(
+//         ".formInput"
+//     );
 
-	if (e.key == "Enter") {
-		setTimeout(() => {
-			createItem(
-				inputs[0].value,
-				inputs[1].value,
-				savings,
-				inputs[2].value,
-				inputs[3].value
-			);
-			inputs[0].value = "";
-			inputs[1].value = "";
-			inputs[2].value = "";
-			inputs[3].value = "";
-		}, 200);
-	}
-});
+//     if (e.key == "Enter") {
+//         setTimeout(() => {
+//             createItem(
+//                 inputs[0].value,
+//                 inputs[1].value,
+//                 savings,
+//                 inputs[2].value,
+//                 inputs[3].value
+//             );
+//             inputs[0].value = "";
+//             inputs[1].value = "";
+//             inputs[2].value = "";
+//             inputs[3].value = "";
+//         }, 200);
+//     }
+// });
 
-function showTotal() {
-	var op = 0;
-	while (op <= 100) {
-		setInterval(() => {
-			console.log("worked");
-		}, 100);
-	}
-}
+// function showTotal() {
+//     var op = 0;
+//     while (op <= 100) {
+//         setInterval(() => {
+//             console.log("worked");
+//         }, 100);
+//     }
+// }
 
-function createItem(name, price, budget, link, img) {
-	var body = document.querySelectorAll(".items")[0];
-	var div = document.createElement("div");
-	var p1 = document.createElement("p");
-	var p2 = document.createElement("p");
-	var p3 = document.createElement("p");
-	var del = document.createElement("button");
-	var linkTag = document.createElement("a");
-	var linkBtn = document.createElement("button");
+// function createItem(
+//     name,
+//     price,
+//     budget,
+//     link,
+//     img
+// ) {
+//     const body = document.querySelectorAll(
+//         ".items"
+//     )[0];
+//     const div = document.createElement(
+//         "div"
+//     );
+//     const p1 = document.createElement(
+//         "p"
+//     );
+//     const p2 = document.createElement(
+//         "p"
+//     );
+//     const p3 = document.createElement(
+//         "p"
+//     );
+//     const del = document.createElement(
+//         "button"
+//     );
+//     const linkTag = document.createElement(
+//         "a"
+//     );
+//     const linkBtn = document.createElement(
+//         "button"
+//     );
 
-	div.classList.add("item");
-	p1.classList.add("top");
-	p2.classList.add("middle");
-	p3.classList.add("bottom");
-	del.classList.add("faBtn");
-	linkTag.setAttribute("target", "_blank");
-	console.log(link);
-	linkTag.setAttribute("href", link);
-	linkBtn.classList.add("faBtn");
-	if (img) {
-		console.log(img);
-		var image = document.createElement("img");
-		image.src = img;
-		image.classList.add("img");
-		div.classList.add("pic");
-		div.classList.remove("item");
-		div.appendChild(image);
-	}
-	var nameSpan = `<span class="value">${name}</span>`;
-	var priceSpan = `<span class="value">£${price}</span>`;
-	var savingsSpan = `<span class="value">£${budget - price}</span>`;
-	p1.innerHTML = `Name: ${nameSpan}`;
-	p2.innerHTML = `Price: ${priceSpan}`;
-	p3.innerHTML = `Amount left: ${savingsSpan}`;
-	del.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-	linkBtn.innerHTML = `<i class='fas fa-link'></i>`;
-	linkTag.appendChild(linkBtn);
+//     div.classList.add("item");
+//     p1.classList.add("top");
+//     p2.classList.add("middle");
+//     p3.classList.add("bottom");
+//     del.classList.add("faBtn");
+//     linkTag.setAttribute(
+//         "target",
+//         "_blank"
+//     );
+//     console.log(link);
+//     linkTag.setAttribute("href", link);
+//     linkBtn.classList.add("faBtn");
+//     if (img) {
+//         console.log(img);
+//         const image = document.createElement(
+//             "img"
+//         );
+//         image.src = img;
+//         image.classList.add("img");
+//         div.classList.add("pic");
+//         div.classList.remove("item");
+//         div.appendChild(image);
+//     }
+//     const nameSpan = `<span class="value">${name}</span>`;
+//     const priceSpan = `<span class="value">£${price}</span>`;
+//     var savingsSpan = `<span class="value">£${
+//         budget - price
+//     }</span>`;
+//     p1.innerHTML = `Name: ${nameSpan}`;
+//     p2.innerHTML = `Price: ${priceSpan}`;
+//     p3.innerHTML = `Amount left: ${savingsSpan}`;
+//     del.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+//     linkBtn.innerHTML = `<i class='fas fa-link'></i>`;
+//     linkTag.appendChild(linkBtn);
 
-	//add text to each item
-	div.appendChild(p1);
-	div.appendChild(p2);
-	div.appendChild(p3);
-	div.appendChild(del);
-	div.appendChild(linkTag);
-	body.appendChild(div);
-	div.classList.add("show");
-	total = total + parseFloat(price);
-	savings -= parseFloat(price);
-	totalView.innerHTML = `${total}`;
-	del.addEventListener("click", function () {
-		setTimeout(() => {
-			this.parentElement.classList.add("hide");
-			sleep(500).then(() => {
-				this.parentElement.remove();
-			});
-			savings += parseFloat(price);
-			total -= parseFloat(price);
-			totalView.innerHTML = `${total}`;
-			if (total == 0) {
-				textShow.classList.remove("show");
-				textShow.classList.add("hide");
-			}
-		});
-	});
-	textShow.classList.add("show");
-}
+//     //add text to each item
+//     div.appendChild(p1);
+//     div.appendChild(p2);
+//     div.appendChild(p3);
+//     div.appendChild(del);
+//     div.appendChild(linkTag);
+//     body.appendChild(div);
+//     div.classList.add("show");
+//     total = total + parseFloat(price);
+//     savings -= parseFloat(price);
+//     totalView.innerHTML = `${total}`;
+//     del.addEventListener(
+//         "click",
+//         function () {
+//             setTimeout(() => {
+//                 this.parentElement.classList.add(
+//                     "hide"
+//                 );
+//                 sleep(500).then(() => {
+//                     this.parentElement.remove();
+//                 });
+//                 savings += parseFloat(
+//                     price
+//                 );
+//                 total -= parseFloat(
+//                     price
+//                 );
+//                 totalView.innerHTML = `${total}`;
+//                 if (total == 0) {
+//                     textShow.classList.remove(
+//                         "show"
+//                     );
+//                     textShow.classList.add(
+//                         "hide"
+//                     );
+//                 }
+//             });
+//         }
+//     );
+//     textShow.classList.add("show");
+// }
 
-createItem("1440p Monitor", 400, "amazon.com", "google.com");
+// createItem(
+//     "1440p Monitor",
+//     400,
+//     "amazon.com",
+//     "google.com"
+// );
