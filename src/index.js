e3e0afcc123bf54ff0d459aaf69b919f8dd89589
Xavier Hazzard <xavier@hazzard.org.uk>
2021-04-19 18:22:39 +0100

    Recompiled

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const PORT = process.env.PORT || 5005;
-const options = {};
+const PORT = 5005;
 const items = [];
 let budget = 5000;
 let total = 0;
 //    middleware
-app.use(express_1.default.static("public", options));
+app.use(express_1.default.static("public"));
 app.use(express_1.default.urlencoded({
     extended: true,
 }));
